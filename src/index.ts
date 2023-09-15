5cf4b5358f945ab4db260abc8394eacbd71d839b
fe-xiaoxin <zxin088@gmail.com>
2023-09-15 13:58:13 +0800

    feat(app): 支持导出更多模块

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ export * from './command/cheanup';
 export * from './command/git-commit';
 export * from './command/git-commit-verify';
 export * from './command/git-init-hooks';
+
+export * from './shared/index';
+export * from './shared/config';
+export * from './shared/types';
+
+export * from './setup';
