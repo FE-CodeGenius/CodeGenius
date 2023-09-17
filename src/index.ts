294ba3810af9359261864fc348f0c5ab26373595
小鑫同学 <zxin088@gmail.com>
2023-09-17 14:53:14 +0800

    fix(build): 修复模块导出的范围

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from './command/cheanup';
 export * from './command/git-commit';
 export * from './command/git-commit-verify';
 export * from './command/git-init-hooks';
+export * from './command/npm-run';
+export * from './command/eslint-fix';
 
 export * from './shared/index';
 export * from './shared/config';
