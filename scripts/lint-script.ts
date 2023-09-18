94037a47419dfe8a313da727eb7e7c187b6722e2
fe-xiaoxin <zxin088@gmail.com>
2023-09-18 15:30:49 +0800

    🚧build(build): 优化构建流程

--- scripts/lint-script.ts
+++ scripts/lint-script.ts
@@ -1,12 +1,11 @@
 import { eslintFix } from "./../src/command/eslint-fix";
-import { execCommand, loggerWarring } from "../src/shared";
 import { prettierFormat } from "../src/command/prettier-format";
+import { loggerWarring } from "../src/shared";
 
 async function lint() {
   try {
     await prettierFormat(["./src/", "./scripts/"]);
     await eslintFix(["./src/", "./scripts/"]);
-    await execCommand("git", ["add", "."]);
   } catch (error) {
     loggerWarring(error);
   }
