afa0367286d7635a76f1a243e133fc469e17c73e
fe-xiaoxin <zxin088@gmail.com>
2023-09-19 16:56:24 +0800

    🔧chore(core): 修复执行脚本

--- scripts/lint-script.ts
+++ scripts/lint-script.ts
@@ -1,14 +1,16 @@
 import { eslintFix } from "@/command/eslint-fix";
 import { prettierFormat } from "@/command/prettier-format";
-import { execCommand, loggerWarring } from "@/shared";
+import { execCommand, printError } from "@/shared";
 
 async function lint() {
   try {
     await prettierFormat(["./src/", "./scripts/"]);
-    await execCommand("git", ["add", "."]);
+    await execCommand("git", ["add", "."], {
+      stdio: "inherit",
+    });
     await eslintFix(["./src/", "./scripts/"]);
   } catch (error) {
-    loggerWarring(error);
+    printError(error);
   }
 }
 
